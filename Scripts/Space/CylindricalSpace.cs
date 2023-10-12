e9304e0e385da66eb12268b337be2b888962b1dc
Joshua Corvinus <dude.dudeness@gmail.com>
2023-10-12 18:01:02 -0500

    fixing curve for space warping

--- Scripts/Space/CylindricalSpace.cs
+++ Scripts/Space/CylindricalSpace.cs
@@ -14,7 +14,7 @@ namespace Instrumental.Space
 		{
 			Vector3 anchorDelta;
 			Vector3 anchorRectPos = this.transform.InverseTransformPoint(Origin.position);
-			anchorDelta = anchorRectPos - localPoint;
+			anchorDelta = localPoint - anchorRectPos;
 
 			float angle = anchorDelta.x / this.radius;
 			float height = anchorDelta.y;
@@ -49,7 +49,7 @@ namespace Instrumental.Space
 		{
 			Vector3 anchorDelta;
 			Vector3 anchorRectPos = this.transform.InverseTransformPoint(Origin.position);
-			anchorDelta = anchorRectPos - localPosition;
+			anchorDelta = localPosition - anchorRectPos;
 
 			float angle = anchorDelta.x / this.radius;
 
@@ -72,7 +72,7 @@ namespace Instrumental.Space
 			Vector3 anchorDelta = Vector3.zero;
 			Vector3 anchorRectPos = this.transform.InverseTransformPoint(Origin.position);
 
-			anchorDelta = anchorRectPos - localPosition;
+			anchorDelta = localPosition - anchorRectPos;
 			float angle = anchorDelta.x / this.radius;
 
 			Quaternion rotation = Quaternion.Euler(0, angle * Mathf.Rad2Deg, 0);
