09a20fd464f9a3a3364cf318bcab67ae86073638
Joshua Corvinus <dude.dudeness@gmail.com>
2023-10-30 00:13:59 -0500

    moving more stuff

--- Scripts/Modeling/Test/PolygonTest.cs
+++ Scripts/Modeling/Test/PolygonTest.cs
@@ -7,9 +7,6 @@ public class PolygonTest : MonoBehaviour
     [Range(3, 32)]
     [SerializeField] int vertCount = 3;
 
-    Vector3[] verts;
-    float angleIncrement;
-
     public int VertCount { get { return vertCount; } }
 
     // Start is called before the first frame update
@@ -26,28 +23,6 @@ public class PolygonTest : MonoBehaviour
 
 	private void OnDrawGizmos()
 	{
-        verts = new Vector3[vertCount];
 
-        angleIncrement = 360 / vertCount;
-
-        for(int i=0; i < vertCount; i++)
-		{
-            Vector3 point = Vector3.up;
-
-            Quaternion rotatiton = Quaternion.AngleAxis(angleIncrement * i, Vector3.forward);
-            point = rotatiton * point;
-            verts[i] = point;
-		}
-
-        bool canQuad = (vertCount % 2 == 0);
-        Gizmos.color = (canQuad) ? Color.green : Color.red;
-        for(int i=0; i < vertCount; i++)
-		{
-            if (i < vertCount - 1) Gizmos.DrawLine(verts[i], verts[i + 1]);
-            else Gizmos.DrawLine(verts[i], verts[0]);
-		}
-
-        // we need to figure out how many triangles we need
-        // then figure out connectivity for each one
 	}
 }
