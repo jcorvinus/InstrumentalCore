c47bd455fdd18a939ae56b8ab635391885f24f43
Joshua Corvinus <dude.dudeness@gmail.com>
2023-12-07 23:09:20 -0600

    Event relay stuff

--- VRkeyboard/Scripts/KeyButton.cs
+++ VRkeyboard/Scripts/KeyButton.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using TMPro;
 using Instrumental.Controls;
 
 namespace VRKeyboard
@@ -18,17 +19,9 @@ namespace VRKeyboard
         public event KeyButtonEventHandler HoverLost;
 
         private Keyboard keyboard;
-        private ButtonRuntime button;
 
-        private bool upperCase = false;
-        public bool UpperCase { get { return upperCase; } set { upperCase = value; SetText(); } }
-
-        private KeyCode key;
-        public KeyCode Key
-        {
-            get { return key; }
-            set { key = value; SetText(); }
-        }
+		#region Phys Keyboard Stuff
+		private ButtonRuntime button;
 
         private UnityEngine.UI.Text characterText;
         public string Text
@@ -40,13 +33,54 @@ namespace VRKeyboard
             get { return characterText; }
             set { characterText = value; }
         }
+        #endregion
+
+        #region Fast Keyboard Stuff
+        UnityEngine.UI.Button unityButton;
+        UnityEngine.EventSystems.EventTrigger unityEventTrigger;
+        UnityEngine.EventSystems.EventTrigger.Entry hoverStartEntry;
+        UnityEngine.EventSystems.EventTrigger.Entry hoverEndEntry;
+
+        TMP_Text tmpText;
+		#endregion
+
+		private bool upperCase = false;
+        public bool UpperCase { get { return upperCase; } set { upperCase = value; SetText(); } }
+
+        private KeyCode key;
+        public KeyCode Key
+        {
+            get { return key; }
+            set { key = value; SetText(); }
+        }
 
         void Awake()
         {
-            button = GetComponent<ButtonRuntime>();
             keyboard = GetComponentInParent<Keyboard>();
 
-            if(keyboard == null)
+            button = GetComponent<ButtonRuntime>();
+
+            unityButton = GetComponent<UnityEngine.UI.Button>();
+            tmpText = GetComponentInChildren<TMP_Text>();
+
+            if(unityButton)
+			{
+                unityEventTrigger = unityButton.gameObject.AddComponent<UnityEngine.EventSystems.EventTrigger>();
+                hoverStartEntry = new UnityEngine.EventSystems.EventTrigger.Entry()
+                {
+                    eventID = UnityEngine.EventSystems.EventTriggerType.PointerEnter,
+                };
+                hoverStartEntry.callback.AddListener((data) => { OnPointerEnter((UnityEngine.EventSystems.PointerEventData)data); });
+
+                hoverEndEntry = new UnityEngine.EventSystems.EventTrigger.Entry()
+                {
+                    eventID = UnityEngine.EventSystems.EventTriggerType.PointerExit
+                };
+                hoverEndEntry.callback.AddListener((data) => { OnPointerExit((UnityEngine.EventSystems.PointerEventData)data); });
+
+            }
+
+			if (keyboard == null)
             {
                 enabled = false;
                 return;
@@ -57,9 +91,20 @@ namespace VRKeyboard
         {
             SetText();
 
-            button.ButtonActivated += OnButtonActivated;
-            button.ButtonHovered += OnButtonHoverGained;
-            button.ButtonHoverEnded += OnButtonHoverLost;
+            if (button)
+            {
+                button.ButtonActivated += OnButtonActivated;
+                button.ButtonHovered += OnButtonHoverGained;
+                button.ButtonHoverEnded += OnButtonHoverLost;
+            }
+
+            if(unityButton)
+			{
+                unityButton.onClick.AddListener(Activate);
+
+                unityEventTrigger.triggers.Add(hoverStartEntry);
+                unityEventTrigger.triggers.Add(hoverEndEntry);
+			}
         }
 
         private void SetText()
@@ -79,9 +124,19 @@ namespace VRKeyboard
 
         void OnDisable()
         {
-            button.ButtonActivated -= OnButtonActivated;
-            button.ButtonHovered -= OnButtonHoverGained;
-            button.ButtonHoverEnded -= OnButtonHoverLost;
+            if (button)
+            {
+                button.ButtonActivated -= OnButtonActivated;
+                button.ButtonHovered -= OnButtonHoverGained;
+                button.ButtonHoverEnded -= OnButtonHoverLost;
+            }
+
+            if(unityButton)
+			{
+                unityButton.onClick.RemoveListener(Activate);
+                unityEventTrigger.triggers.Remove(hoverStartEntry);
+                unityEventTrigger.triggers.Remove(hoverEndEntry);
+			}
         }
 
         private void Update()
@@ -89,20 +144,45 @@ namespace VRKeyboard
             button.VolumeModifier = keyboard.VolumeAdjust;
         }
 
-        #region Event Methods
+        void Activate()
+		{
+            if (Activated != null) Activated(this);
+        }
+
+        void StartHover()
+		{
+            if (HoverGained != null) HoverGained(this);
+        }
+
+        void EndHover()
+		{
+            if (HoverLost != null) HoverLost(this);
+        }
+
+        void OnPointerEnter(UnityEngine.EventSystems.PointerEventData eventData)
+		{
+            StartHover();
+		}
+
+        void OnPointerExit(UnityEngine.EventSystems.PointerEventData eventData)
+		{
+            EndHover();
+		}
+
+        #region Phys Event Methods
         private void OnButtonActivated(ButtonRuntime sender)
         {
-            if (Activated != null) Activated(this);
+            Activate();
         }
 
         private void OnButtonHoverGained(ButtonRuntime sender)
         {
-            if (HoverGained != null) HoverGained(this);
+            StartHover();   
         }
 
         private void OnButtonHoverLost(ButtonRuntime sender)
         {
-            if (HoverLost != null) HoverLost(this);
+            EndHover();   
         }
         #endregion
     }
